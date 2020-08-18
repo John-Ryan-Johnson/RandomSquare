6150f0ff5c8dc769b3279f4c48e08f9074cc0826
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-08-18 16:57:49 -0500

    Add project files.

---
+++ RandomSquare/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            var randomNumberList = new List<int>();
+
+            var squaredNumberList = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                var randomNumber = random.Next(1, 51);
+                randomNumberList.Add(randomNumber);
+                squaredNumberList.Add(randomNumber * randomNumber);
+            }
+            Console.WriteLine($"Random numbers: {String.Join(", ", randomNumberList)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Squared random numbers: {String.Join(", ", squaredNumberList)}");
+
+            Console.WriteLine();
+
+            var evenSquaredNumbers = new List<int>();
+
+            foreach (var number in squaredNumberList)
+            {
+                if (number % 2 == 0)
+                {
+                    evenSquaredNumbers.Add(number);
+                }
+            }
+
+            Console.WriteLine($"Even squared numbers: {String.Join(", ", evenSquaredNumbers)}");
+        }
+    }
+}
